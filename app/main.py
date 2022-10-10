437b2197e21b29c081c867e094e076a3fd9e893d
guyacamole <guy.xd2000@gmail.com>
2022-10-09 19:06:01 -0500

    dios

--- app/main.py
+++ app/main.py
@@ -24,10 +24,10 @@ async def identification(file: UploadFile = File(...)):
 
     En esta ruta vamos a recibir las identificaciones
     """
-    async with aiofiles.open("app/tmp.jpg", 'wb') as out_file:
+    async with aiofiles.open("app/imgAPI/0.jpg", 'wb') as out_file:
         content = await file.read()  # async read
         await out_file.write(content)  # async write
-        aux = id.ineToJson("app/tmp.jpg")
+        aux = id.ineToJson("app/imgAPI/0.jpg")
         print(aux)
         return aux
 
