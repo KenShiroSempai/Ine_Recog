5c6b078def029492167e95bdfbbb83a70f6d7b33
guyacamole <guy.xd2000@gmail.com>
2022-11-01 13:45:58 -0600

    whats

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from requests.auth import HTTPDigestAuth
 from pydantic import BaseModel
 import qrcode
 from PIL import Image
+import pathlib
+import os
 
 app = FastAPI()
 
@@ -28,36 +30,21 @@ async def root():
     return {"Estado": "Funcionando"}
 
 
-@app.get("/plumas/{ip}")
-async def openIP(ip):
-    """Monitorear el procesamiento.
-
-    Metodo exclusivo para regresar las imagenes
-    de tal modo de ver los reultados
-    """
-    try:
-        requests.get("http://"+ip+"/axis-cgi/io/port.cgi?action=2%3A%2F500%5C", auth=HTTPDigestAuth('root', 'mfmssmcl'))
-    except Exception as ex:
-        return {"error": ex.args}
-    return {"msg": "OK"}
-
-
 @app.post("/upload")
 async def identification(file: UploadFile = File(...)):
     """Subir identificaciones.
 
-    En esta ruta vamos a recibir las identificaciones
+    En esta ruta vamos a recibir las identificaciones y retorna un JSON con los datos de la Identificacion
     """
     async with aiofiles.open("app/imgAPI/0.jpg", 'wb') as out_file:
         content = await file.read()  # async read
         await out_file.write(content)  # async write
         aux = id.ineToJson("app/imgAPI/0.jpg")
-        print(aux)
         return aux
 
 
 @app.get("/img/{photo}")
-async def returnImg(photo):
+async def get_Img(photo):
     """Monitorear el procesamiento.
 
     Metodo exclusivo para regresar las imagenes
@@ -73,7 +60,12 @@ async def returnImg(photo):
 
 
 @app.post("/qr/")
-async def create_item(item: Item):
+async def create_qrImg(item: Item):
+    """Generador de QR.
+
+    Metodo para uso exclusivo del bot de whatsapp, recibe un string y genera un codigo qr con dicho string
+    lo guarda en la carpeta espejeada en disk2 para que pueda ser tomado por otro docker 
+    """
     if item.url:
         QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
         url = item.url
@@ -83,3 +75,29 @@ async def create_item(item: Item):
         QRimg = QRimg.resize((1000,1000),Image.ANTIALIAS)
         QRimg.save(r'app/qr/QrWhats.png')
     return {"msg": "too cool "}
+
+@app.post("/plumas/")
+async def abrir_plumas(item: Item):
+    """Abrir plumas de emergencia.
+
+    Metodo creado para abrir plumas, recibe una ip y manda el pulso a la camara para
+    """
+    try:
+        requests.get("http://"+item.url+"/axis-cgi/io/port.cgi?action=2%3A%2F500%5C", auth=HTTPDigestAuth('root', 'mfmssmcl'))
+        print (item.url)
+    except Exception as ex:
+        return {"error": ex.args}
+    return {"msg": "OK"}
+
+@app.get("/delete/")
+async def create_item():
+    """Borrar Qr generado.
+
+    Metodo para uso exclusivo de el bot de whatsapp, borra la imagen que este generada
+    en caso de que no exista el archivo, retorna un mensaje diciendo que no existe.
+    """
+    file_to_rem = pathlib.Path("app/qr/QrWhats.png")
+    if(os.path.isfile(file_to_rem)):
+        file_to_rem.unlink()
+        return {"msg":"Archivo eliminado"}
+    return {"msg":"No hay archivo para eliminar"}
