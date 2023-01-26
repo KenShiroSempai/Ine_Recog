5d66ab970feb6010653e5cf1a7ca1ce35334efa4
guyacamole <guy@cs3.mx>
2023-01-26 00:29:56 -0600

    oda

--- app/main.py
+++ app/main.py
@@ -141,7 +141,7 @@ async def logEnramada(item: logEnramada):
                     cv2.imread("app/imgAPI/0.jpg"))
     else:
         name = aux["name"]
-    newPath = "app/" + conjunto+"/"+building
+    newPath = "app/" + conjunto+"/"+building+"/"+floor
 
     idPath = "/" + str(timestamp)+"idCard" + "_" + name + ".png"
     facePath = "/" + str(timestamp)+"face" + "_" + name + ".png"
