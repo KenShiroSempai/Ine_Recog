a69ec29d50a7c4564db2f1a0bf6eb0aa4648bfe4
guyacamole <guy@cs3.mx>
2023-08-17 11:32:43 -0600

    test1

--- app/extras/globalData.py
+++ app/extras/globalData.py
@@ -9,7 +9,7 @@ FAILPATH = IMGPATH + 'fail/'
 DIC OF TEMPLATES AND HIS IMPORTANT POINTS
 '''
 TEMPLATE = 'templates/'
-TEMPLATES = {'template_0.jpeg': [(488, 413), (784, 462), (300, 160), (665, 294)],
+TEMPLATES = {'template_0.jpeg': [(494, 413), (784, 462), (300, 160), (665, 294)],
              'ine1.jpeg': [(505, 433), (784, 476), (315, 175), (655, 303)],
              'ife.jpeg': [(10, 433), (511, 472), (0, 182), (365, 289)],
              'lic.jpeg': [(128, 1354), (589, 1545), (146, 768), (854, 975)]
